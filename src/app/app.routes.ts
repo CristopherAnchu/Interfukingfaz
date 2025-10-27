6e80aa5b6f000b889bf84125ac07aa4ffc058e63
marco <marcoadrianzambra@gmail.com>
2025-10-26 22:20:45 -0500

    add: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
 import { AdminComponent } from './componentes/admin/admin.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { FeedComponent } from './componentes/feed/feed.component';
+import { ProfileComponent } from './componentes/profile/profile.component';
 
 //Importaciones de los guards
 import { adminGuard } from './guards/admin.guard';
@@ -15,6 +17,8 @@ export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch: "full"},
     {path: "login", component: LoginComponent},
     {path: "admin", component: AdminComponent, canActivate:[loginGuard, adminGuard]},
+    {path: "feed", component: FeedComponent, canActivate:[loginGuard]},
+    {path: "profile", component: ProfileComponent, canActivate:[loginGuard]},
     {path: "home", component: HomeComponent, canActivate:[loginGuard]},
     {path: "**", redirectTo: "login"},
 ];
